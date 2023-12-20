d9bbcc69d6376707c3cacd444fde2b93c2e49c1d
yon173089350 <58987894+yon173089350@users.noreply.github.com>
2023-12-20 22:15:18 +0800

    init

--- lark-monitor/src/main/java/larkisland/monitor/App.java
+++ lark-monitor/src/main/java/larkisland/monitor/App.java
@@ -1,20 +1,26 @@
 package larkisland.monitor;
 
 import io.quarkus.runtime.StartupEvent;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import larkisland.monitor.job.JobFactory;
 import lombok.extern.log4j.Log4j2;
-import org.quartz.Scheduler;
 
 @ApplicationScoped
 @Log4j2
 public class App {
+
+    private final JobFactory jobFactory;
+
     @Inject
-    Scheduler quartz;
+    public App(JobFactory jobFactory) {
+        this.jobFactory = jobFactory;
+    }
 
     void onStart(@Observes StartupEvent ev) {
-        log.info(ev.toString());
-        log.info(quartz.toString());
+        log.info("Init App: {}", ev.toString());
+        jobFactory.init();
     }
 }
---
+++ lark-monitor/src/main/java/larkisland/monitor/job/JobFactory.java
@@ -0,0 +1,22 @@
+package larkisland.monitor.job;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.extern.log4j.Log4j2;
+import io.quarkus.scheduler.Scheduler;
+
+@ApplicationScoped
+@Log4j2
+public class JobFactory {
+    private final Scheduler scheduler;
+
+    @Inject
+    public JobFactory(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    public void init() {
+        log.info(scheduler.toString());
+    }
+
+}
