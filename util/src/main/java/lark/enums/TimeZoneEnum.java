43557e21c62abcc08c1d8e84072fcf5cdbaa338e
yon173089350 <58987894+yon173089350@users.noreply.github.com>
2024-01-22 23:02:39 +0800

    re-structure

--- util/src/main/java/lark/enums/TimeZoneEnum.java
+++ util/src/main/java/lark/enums/TimeZoneEnum.java
@@ -3,21 +3,21 @@ package lark.enums;
 import java.time.ZoneId;
 
 public enum TimeZoneEnum {
-    HK_TIMEZONE("HK","Asia/Hong_Kong"),
-    UK_TIMEZONE("UK","Europe/London");
+    HK_TIMEZONE("HK", "Asia/Hong_Kong"),
+    UK_TIMEZONE("UK", "Europe/London");
 
     private final String code;
 
     private final String zone;
 
     TimeZoneEnum(String code, String zone) {
-        this.code=code;
-        this.zone=zone;
+        this.code = code;
+        this.zone = zone;
     }
 
-    public static ZoneId getZoneId(String code){
-        for(TimeZoneEnum item: TimeZoneEnum.values()){
-            if(item.getCode().contains(code)){
+    public static ZoneId getZoneId(String code) {
+        for (TimeZoneEnum item : TimeZoneEnum.values()) {
+            if (item.getCode().contains(code)) {
                 return ZoneId.of(item.getZone());
             }
         }
