91b8346511583b02aa233d64a0810aeedbe907df
yon173089350 <58987894+yon173089350@users.noreply.github.com>
2025-03-21 14:57:39 +0800

    Update MonitorListener.java

--- monitor/src/main/java/lark/island/monitor/MonitorListener.java
+++ monitor/src/main/java/lark/island/monitor/MonitorListener.java
@@ -7,7 +7,6 @@ import jakarta.inject.Inject;
 import lark.island.database.util.DatastoreFactory;
 import lark.island.monitor.job.CronManager;
 import lombok.extern.log4j.Log4j2;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 @Log4j2
@@ -19,21 +18,9 @@ public class MonitorListener {
         this.cronManager = cronManager;
     }
 
-    @ConfigProperty(name = "db.url")
-    String jdbcUrl;
-
-    @ConfigProperty(name = "db.user")
-    String username;
-
-    @ConfigProperty(name = "db.pw")
-    String password;
-
-    @ConfigProperty(name = "db.driver")
-    String driverClassName;
-
     void onStart(@Observes StartupEvent ev) {
         log.info("Init App: {}", ev.toString());
-        DatastoreFactory.init(jdbcUrl, username, password, driverClassName);
+        DatastoreFactory.init();
         cronManager.init();
     }
 }
